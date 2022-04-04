50fceb060727f053777f2afa727b34468db4f98b
HossainRafi <hossainrafi71@gmail.com>
2022-04-04 09:56:51 +0600

    header completed

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Home/Home";
 import Reviews from "./Reviews/Reviews";
-import Dashboard from "./Dashboard/Dashboard";
+import DashBoard from "./DashBoard/DashBoard";
 import Blogs from "./Blogs/Blogs";
 import About from "./About/About";
-import Error from './Error/Error';
+import Error from "./Error/Error";
 import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashBoard" element={<DashBoard />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/about" element={<About />} />
-        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-      <div>
-        <h1 className="text-center text-blue-700 text-6xl">Header Page</h1>
-      </div>
-    );
+  return (
+    <div className="text-xl font-semibold bg-gray-200 h-[3.5rem] flex justify-center items-center gap-6">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "text-blue-600" : "text-slate-600 hover:text-blue-600"
+        }
+      >
+        HOME
+      </NavLink>
+      <NavLink
+        to="/reviews"
+        className={({ isActive }) =>
+          isActive ? "text-blue-600" : "text-slate-600 hover:text-blue-600"
+        }
+      >
+        REVIEWS
+      </NavLink>
+      <NavLink
+        to="/dashBoard"
+        className={({ isActive }) =>
+          isActive ? "text-blue-600" : "text-slate-600 hover:text-blue-600"
+        }
+      >
+        DASHBOARD
+      </NavLink>
+      <NavLink
+        to="/blogs"
+        className={({ isActive }) =>
+          isActive ? "text-blue-600" : "text-slate-600 hover:text-blue-600"
+        }
+      >
+        BLOGS
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) =>
+          isActive ? "text-blue-600" : "text-slate-600 hover:text-blue-600"
+        }
+      >
+        ABOUT
+      </NavLink>
+    </div>
+  );
 };
 
 export default Header;
